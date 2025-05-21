a51a2b22561a8ff4f0efe0543c2d9ceb0ed097e8
fengjianxiaotie <2956769008@qq.com>
2025-05-21 20:29:51 +0800

    2025-5-21所完成的内容,基本脚手架已完成

---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/domain/dataobject/UserDO.java
@@ -0,0 +1,54 @@
+package com.quanxiaoha.xiaohashu.auth.domain.dataobject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDO {
+    private Long id;
+
+    private String username;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
